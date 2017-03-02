881c23ba39821440d0c605d130561dc1b6c95ee8
Steven Brodhead <keldor314@gmail.com>
2017-03-02 09:17:35 -0700

    Work on <animation> in file format

--- Flam35/Tests/HexStringToNumberString.fsx
+++ Flam35/Tests/HexStringToNumberString.fsx
@@ -1,25 +1,27 @@
 ﻿open System
 open System.Text.RegularExpressions
 
+
+let partitionSeq width sequence =
+  sequence
+  |> Seq.windowed width
+  |> Seq.mapi (fun i n -> (i,n))
+  |> Seq.filter (fun (i,n) -> i%width = 0)
+  |> Seq.map (fun (_,a) -> a)
+
 let hexStringToNumberString str =
     Regex.Replace (str, "\W", "")
-    |> Seq.windowed 2
-    |> Seq.mapi (fun i n -> (i,n))
-    |> Seq.filter (fun (i,n)-> i%2=0)
-    |> Seq.map (fun (_,a) -> new string(a))
+    |> partitionSeq 2
+    |> Seq.map (fun a -> new string(a))
     |> Seq.map (fun str -> UInt32.Parse(str,System.Globalization.NumberStyles.AllowHexSpecifier))
     |> Seq.map (fun num -> num.ToString ())
-    |> Seq.windowed 3
-    |> Seq.mapi (fun i n -> (i,n))
-    |> Seq.filter (fun (i,n)-> i%3=0)
-    |> Seq.map (fun (_,a) -> 
+    |> partitionSeq 3
+    |> Seq.map (fun a -> 
         a 
         |> Array.map (fun numStr -> numStr.PadRight(4))
         |> Array.reduce (+))
-    |> Seq.windowed 8
-    |> Seq.mapi (fun i n -> (i,n))
-    |> Seq.filter (fun (i,n)-> i%8=0)
-    |> Seq.map (fun (_,a) -> 
+    |> partitionSeq 8
+    |> Seq.map (fun a -> 
         a 
         |> Array.map (fun numStr -> numStr.PadRight(16))
         |> Array.reduce (+))
