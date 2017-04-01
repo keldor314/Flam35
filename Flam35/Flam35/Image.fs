82e84bd6e1685197a6bfc34c68e327274d8d02f7
Steven Brodhead <keldor314@gmail.com>
2017-04-01 04:59:22 -0600

    Added image encoder for output.

---
+++ Flam35/Flam35/Image.fs
@@ -0,0 +1,21 @@
+﻿module Image
+
+open System.IO
+open System.Windows.Media
+open System.Windows.Media.Imaging
+
+
+let writeImage (buffer: float32 []) width height useAlpha file =
+    let format = if useAlpha then PixelFormats.Rgba128Float else PixelFormats.Rgb128Float
+    let source = 
+        BitmapSource.Create ( width, height,
+                              96., 96.,
+                              format,
+                              null,
+                              buffer,
+                              16*width )
+    let encoder = new WmpBitmapEncoder ()
+    let output = new FileStream(file, FileMode.OpenOrCreate)
+    encoder.Frames.Add <| BitmapFrame.Create source
+    encoder.Save output
+    ()
